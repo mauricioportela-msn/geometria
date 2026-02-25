4dd7164fde079534062bfe82adc0c0888fe341a6
mauricioportela-msn <mauricioportela@msn.com>
2026-02-25 17:14:02 -0300

    Update demo_geometria.py

--- python/demo_geometria.py
+++ python/demo_geometria.py
@@ -18,34 +18,34 @@ def main():
     angulo = 60
 
     print("Quadrado:")
-    print("Área:", geo.area_quadrada(lado))
-    print("Perímetro:", geo.perimetro_do_quadrado(lado))
-    print("Diagonal:", geo.comprimento_diagonal_quadrado(lado))
+    print(f"Área: {geo.area_quadrada(lado)}")
+    print(f"Perímetro: {geo.perimetro_do_quadrado(lado)}")
+    print(f"Diagonal: {geo.comprimento_diagonal_quadrado(lado)}")
     print()
 
     print("Retângulo:")
-    print("Área:", geo.area_retangular(comprimento, largura))
-    print("Diagonal:", geo.comprimento_diagonal_retangulo(comprimento, largura))
+    print(f"Área: {geo.area_retangular(comprimento, largura)}")
+    print(f"Diagonal: {geo.comprimento_diagonal_retangulo(comprimento, largura)}")
     print()
 
     print("Círculo:")
-    print("Área:", geo.area_circular(diametro))
-    print("Circunferência:", geo.circunferencia_do_circulo(diametro))
+    print(f"Área: {geo.area_circular(diametro)}")
+    print(f"Circunferência: {geo.circunferencia_do_circulo(diametro)}")
     print()
 
     print("Setor Circular:")
-    print("Área do setor:", geo.area_setor_circular(raio, angulo))
-    print("Comprimento do arco:", geo.comprimento_arco_do_circulo(raio, angulo))
+    print(f"Área do setor: {geo.area_setor_circular(raio, angulo)}")
+    print(f"Comprimento do arco: {geo.comprimento_arco_do_circulo(raio, angulo)}")
     print()
 
     print("Segmento Circular:")
-    print("Área do segmento:", geo.area_segmento_circular(raio, angulo))
-    print("Comprimento da corda:", geo.comprimento_corda_por_angulo(raio, angulo))
+    print(f"Área do segmento: {geo.area_segmento_circular(raio, angulo)}")
+    print(f"Comprimento da corda: {geo.comprimento_corda_por_angulo(raio, angulo)}")
     print()
 
     print("Elipse (20 x 10):")
-    print("Área:", geo.area_elipse(20, 10))
-    print("Circunferência:", geo.circunferencia_elipse(20, 10))
+    print(f"Área: {geo.area_elipse(20, 10)}")
+    print(f"Circunferência: {geo.circunferencia_elipse(20, 10)}")
 
 
 if __name__ == "__main__":
